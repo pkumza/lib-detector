887c077b1e49f409eea05c1f960e3e32e5cb9a36
maziang <pkumza@163.com>
2015-08-20 09:40:48 +0800

    修改了compare.py 希望不要再卡退。

--- hash_package/compare.py
+++ hash_package/compare.py
@@ -15,7 +15,8 @@
         修改intodb.conf的设置，然后运行本代码，或者import intodb.py之后调用main_func()方法即可。
 
     Trivial Notes：
-        dep_packages
+        dep_packages 是第一次尝试，以卡机失败告终。
+
 """
 __author__ = 'Zachary Marv - 马子昂'
 
@@ -91,6 +92,7 @@ def main_func():
         ('b_total_num', pymongo.ASCENDING), ("b_hash", pymongo.ASCENDING)])
     # package = packages.next()
     dep_packages = db[get_config('database', 'db_dep')]
+    dep_packages_st_6 = db[get_config('database', 'db_dep_6')]
 
     """
         Search the packages among the database.
@@ -117,9 +119,12 @@ def main_func():
     status_3_cnt = 0                # Status 3 means lib_child
     status_4_cnt = 0                # Status 4 means directory_root
     status_5_cnt = 0                # Status 5 形如一个文件夹，com，只有一个子文件夹com/admob
+    status_6_cnt = 0                # Status 6 means 有重复，且不是代表元素
     dir_parent_dict = {}            # scanned packages' statuses.
-    package_count = -1              # count
+    package_count = 0               # count
     packages_num = packages.count()
+    cur_p = None
+    cur_b = False
 
     for package in packages:
         package_count += 1
@@ -130,6 +135,7 @@ def main_func():
             del package['_id']
             dep_packages.insert(package)
             status_4_cnt += 1
+            cur_b = False
             continue
 
         # If a package has only one directory, and
@@ -139,10 +145,14 @@ def main_func():
             del package['_id']
             dep_packages.insert(package)
             status_5_cnt += 1
+            cur_b = False
             continue
 
-        # if this package's dir_parent is lib, there's no need to compare this package any more.
-        # 1 means lib_root; 3 means lib_child, so I use Modulo operation here.
+        # (if this package's dir_parent is lib, there's no need to compare this package any more.
+        # 1 means lib_root; 3 means lib_child, so I use Modulo operation here.)
+
+        # 2015/08/20 We decide to compare package no matter what its parent is.
+        '''
         dir_split = package['path'].split('\\')
         dir_parent_split = dir_split[:len(dir_split)-1]
         dir_parent = '\\'.join(dir_parent_split)
@@ -154,7 +164,36 @@ def main_func():
             del package['_id']
             dep_packages.insert(package)
             continue
+        '''
 
+        if not cur_b:       # if cur_package is not available
+            cur_p = package
+            cur_b = True
+            continue
+
+        if package_count != packages_num:
+            if package['depth'] == cur_p['depth']:
+                if package['b_total_call'] == cur_p['b_total_call']:
+                    if package['b_total_num'] == cur_p['b_total_num']:
+                        if package['b_hash'] == cur_p['b_hash']:
+                            cur_p['dep_num'] += 1
+                            package['parent'] = cur_p['path']
+                            package['status'] = 6
+                            status_6_cnt += 1
+                            dep_packages_st_6.insert(package)
+                            continue
+
+        if cur_p['dep_num'] > 50:
+            cur_p['status'] = 1
+            status_1_cnt += 1
+        else:
+            cur_p['status'] = 2
+            status_2_cnt += 1
+
+        dep_packages.insert(cur_p)
+        cur_p = package
+
+        """   *************************************************************
         # len(common) == 0 is used just for the first package comes here.
         # the code in the if-statement will only runs once.
         if len(common) == 0:
@@ -207,11 +246,14 @@ def main_func():
                 total_call_plat = package['b_total_call']
                 total_num_plat = package['b_total_num']
                 common.append({"package": package, "dep_num": 1, "parent": 0})
+
+                ****************************************************************************"""
     """
     This is a copy of code. I put the code here to deal with the last package.
     放在这里用来处理最后一个common list，因为上面的for循环的话，最后一个common list就没办法处理了。
     这就是为什么之前的代码，会发现rep-packages4比packages总量上少一个。
     理论上这些代码只运行一次。
+    """
     """
     for i in common:
         if i['parent'] != 0:
@@ -246,6 +288,9 @@ def main_func():
         package_featured['dep_num'] = i['dep_num']
         dep_packages.insert(package_featured)
     del common[:]
+    """
+
+
     """
     At last, output some statistics into txt file.
     dep_statistics represents the repetitions status.
